6538923056e895a237d6a40b82660c73d88347af
onoshkinivan <98639022+onoshkinivan@users.noreply.github.com>
2023-03-25 15:44:20 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -15,37 +15,36 @@
 //     this.classList.toggle('done');
 // })
 
-// let i2 = document.querySelector('fa-solid fa-trash-can')
-// i2.append('item__btns')
 
-// i2.addEventListener('click', function () {
-//     i2.parentNode.removeChild('fa-trash-can')
-// })
-
-
-let btn = document.querySelector('#addBtn');
 let inp = document.querySelector('#taskInput');
 let ul = document.querySelector('#list');
+let btn = document.querySelector('#addBtn');
 
-let li = document.createElement('li');
-li.className = 'class1';
-li.textContent = inp.value;
-li.append(ul);
+btn.addEventListener('click', function () {
+    let li = document.createElement('li');
+    li.className = 'item';
+    li.textContent = inp.value;
+    ul.append(li);
+    inp.value = '';
 
-let geb = document.createElement('div');
-geb.append(ul);
-geb.className = 'item__btns';
+    let geb = document.createElement('div');
+    li.append(geb);
+    geb.className = 'item__btns';
 
-let i1 = document.querySelector('.fa-regular .fa-square-check');
-i1.append(geb);
+    let add = document.createElement('i');
+    add.className = 'fa-thin fa-pen';
+    geb.append(add);
 
-i1.addEventListener('click', function () {
-    this.classList.toggle('done');
+    add.addEventListener('click', function () {
+        li.classList.toggle('done');
+    })
+
+    let ge = document.createElement('i');
+    ge.className = 'fa-solid fa-trash-can';
+    geb.append(ge);
+
+    ge.addEventListener('click', function () {
+        li.remove();
+    })
 })
 
-let i2 = document.querySelector('fa-solid fa-trash-can')
-i2.append(geb)
-
-i2.addEventListener('click', function () {
-    i2.parentNode.removeChild('fa-trash-can')
-})
